1acd2f36162e4027f9bb8967f1d23b2c1106a564
OmeSky-Studio <86529265+OmeSky-Studio@users.noreply.github.com>
2023-01-04 08:13:49 +0100

    ajout commantaire et correction

--- src/main/java/fr/omeskystudio/omeskystudio/Main.java
+++ src/main/java/fr/omeskystudio/omeskystudio/Main.java
@@ -5,32 +5,53 @@ import fr.omeskystudio.omeskystudio.component.FolderManager;
 import fr.omeskystudio.omeskystudio.menu.MainMenu;
 
 public class Main {
-
+    
+    /*
+        INIT DEFAULT VAR:
+            Display, Running, MainMenu
+    */
     static DisplayManager window;
     static boolean running = false;
     MainMenu mainMenu = new MainMenu();
 
+    /*
+        Init MAINMENU, SET RUNNING TRUE
+        Vérification si le dossier root et créé
+        Lancment de la fonction loop
+    */
     public void init(){
+        if(!FolderManager.isRootCreated()){
+            FolderManager.createDefaultFolder();
+        }
         mainMenu.init();
         running = true;
         loop();
     }
-    int tick = 0;
+    
+    /*
+        Function update
+            Update MainMenu et DisplayManager
+    */
     public void update(){
-        if(!FolderManager.isRootCreated()){
-            FolderManager.createDefaultFolder();
-        }
         mainMenu.update();
         DisplayManager.update();
         //System.out.println(Display.getDimension().getWidth() +"x"+Display.getDimension().getHeight());
     }
 
+    /*
+        Function loop
+    */
     public void loop(){
         while (running){
             update();
         }
     }
 
+    /*
+        Function pricipale
+        Création de la fenêtre
+        lancement de la fonction Init
+    */
     public static void main(String[] args) {
 
         /*CREATE DISPLAY*/
